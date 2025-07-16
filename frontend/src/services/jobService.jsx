6e378c6834230c79f27e202b1424a0fc7a2c4713
Sankalpa_01 <sankalpaanda@gmail.com>
2025-07-16 17:34:10 +0530

    backend added

--- frontend/src/services/jobService.jsx
+++ frontend/src/services/jobService.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const API = `https://distributed-job-scheduler-2-backend.onrender.com/api/jobs`;
+// Read from environment variable
+const API = `${import.meta.env.VITE_API_BASE_URL}/api/jobs`;
 
 export const getJobs = () => axios.get(API).then((res) => res.data);
+
 export const getJobById = (id) =>
   axios.get(`${API}/${id}`).then((res) => res.data);
+
 export const createJob = (data) =>
   axios.post(API, data).then((res) => res.data);
+
 export const deleteJob = (id) => axios.delete(`${API}/${id}`);
